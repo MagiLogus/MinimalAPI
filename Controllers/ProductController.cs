e4b45ef366421c131d1a19c7ca0b0ab59c480828
Paulo Oliveira <paulo.oliveira.phgo@gmail.com>
2024-07-26 10:23:26 -0300

    iniciado api minimal utilizando mongodb

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using minimalAPIMongo.Domains;
+using minimalAPIMongo.Services;
+using MongoDB.Driver;
+
+namespace minimalAPIMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductController : ControllerBase
+    {
+        /// <summary>
+        /// Armazena os dados de acesso da collection 
+        /// </summary>
+        private readonly IMongoCollection<Product> _product;
+
+        /// <summary>
+        /// Construtor que recebe como dependencia o objeto da classe MongoDbService
+        /// </summary>
+        /// <param name="mongoDbService">Objeto da classe MongoDbService</param>
+        public ProductController(MongoDbService mongoDbService)
+        {
+            //obtem a collection "product"
+            _product = mongoDbService.GetDatabase.GetCollection<Product>("product");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            try
+            {
+                var products = await _product.Find(FilterDefinition<Product>.Empty).ToListAsync();
+                return Ok(products);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Product product)
+        {
+            try
+            {
+                await _product.InsertOneAsync(product);
+                return StatusCode(201, product);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
